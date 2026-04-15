70faadafe38e620f3c687da79a16a70536d7590a
ahmedhamda <ahmed.hamda@isimsf.u-sfax.tn>
2026-04-15 02:29:04 +0100

    first commit

---
+++ supply-service/src/main/java/com/stock/supply_service/SupplyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stock.supply_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyServiceApplication.class, args);
+	}
+
+}
